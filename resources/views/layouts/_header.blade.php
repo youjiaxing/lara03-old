1a6b674b47fd9ed22407914f1633d79a83181e9f
YouJiaXing <287009007@qq.com>
2018-08-15 16:49:57 +0800

    Finish 3.1

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -31,8 +31,19 @@
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">
-                            <span class="user-avatar pull-left" style="margin-right: 8px;margin-top: -5px;"
+                            <span class="user-avatar pull-left" style="margin-right: 8px;margin-top: -5px;">
+                                <img src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+                            </span>
+                            {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">退出登录</a>
+                                <form action="{{ route('logout') }}" id="logout-form" method="POST">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
                     </li>
                 @endguest
             </ul>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@
 Route::get('/', 'PagesController@root')->name('root');
 
 
-//Auth::routes();
+
+
+/************************************ Auth::routes() *********************************************************/
+// Auth::routes();
 // Authentication Routes...
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
@@ -29,5 +32,5 @@ Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm'
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
-
+/************************************************************************************************/
 
