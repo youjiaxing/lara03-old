46775e613f31fe058d954668bd0e73b35d9f422d
YouJiaXing <287009007@qq.com>
2018-09-01 16:09:16 +0800

    fix bug about user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -76,7 +76,7 @@ class User extends Authenticatable
             // 不等于 60，做密码加密处理
             $value = bcrypt($value);
         }
-        $this->attributes['password'] = bcrypt($value);
+        $this->attributes['password'] = $value;
     }
 
     public function setAvatarAttribute($path)
