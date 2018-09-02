ef984e09a0f90f469ca003833db90534d8e7038c
YouJiaXing <287009007@qq.com>
2018-09-02 14:17:26 +0800

    add captcha

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -11,7 +11,20 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
-        $phone = $request->phone;
+        $captcha_key = $request->captcha_key;
+        $captchaData = \Cache::get($captcha_key);
+
+        if (!$captchaData) {
+            $this->response->error("图片验证码失效", 422);
+        }
+
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+            // 验证错误就清除缓存
+            \Cache::forget($captcha_key);
+            $this->response->errorUnauthorized('验证码错误');
+        }
+
+        $phone = $captchaData['phone'];
 
         if (!app()->environment('production')) {
             $code = '1234';
@@ -28,15 +41,16 @@ class VerificationCodesController extends Controller
                 return $this->response->errorInternal($message ?? '短信发送异常');
             }
         }
-            $key = 'verificationCode_' . str_random(15);
-            $expiredAt = now()->addMinutes(10);
-            // 缓存验证码 10分钟过期
-            \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
 
-            return $this->response->array([
-                'key' => $key,
-                'expired_at' => $expiredAt->toDateTimeString(),
-            ])->setStatusCode(201);
+        $key = 'verificationCode_' . str_random(15);
+        $expiredAt = now()->addMinutes(10);
+        // 缓存验证码 10分钟过期
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        return $this->response->array([
+            'key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+        ])->setStatusCode(201);
 
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -26,5 +26,8 @@ $api->version('v1', ['namespace' => 'App\Http\Controllers\Api'], function ($api)
 
         // 用户注册
         $api->post('users', 'UsersController@store')->name('api.users.store');
+
+        // 图片验证码
+        $api->post('captchas', 'CaptchasController@store')->name('api.captchas.store');
     });
 });
