6f16892d5be38bd906c2b2889800baaa32d6f93f
YouJiaXing <287009007@qq.com>
2018-09-06 21:29:24 +0800

    Finish 5.3, user update

--- routes/api.php
+++ routes/api.php
@@ -57,8 +57,12 @@ $api->version('v1', [
         // 需要 token 验证的接口
         $api->group(['middleware' => 'api.auth'], function($api) {
             // 当前登录用户信息
-            $api->get('user', 'UsersController@me')
-                ->name('api.user.show');
+            $api->get('user', 'UsersController@me')->name('api.user.show');
+            //编辑登陆用户信息
+            $api->patch('user', 'UsersController@update')->name('api.user.update');
+
+            // 图片资源
+            $api->post('images', 'ImagesController@store')->name('api.images.store');
         });
     });
 });
